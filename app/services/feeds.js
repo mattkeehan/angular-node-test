9fd690e72aa6ca72f6fba9e0e3d727eade1b0256
MATT KEEHAN <matt.keehan@gmail.com>
2015-01-31 23:02:24 +0000

    tidied up auth attempts feed

--- app/services/feeds.js
+++ app/services/feeds.js
@@ -2,11 +2,19 @@
 
 var mongoConnection = require("../util/connectMongo");
 
+function _cleanData(items) {
+    items.forEach (function (currentVal) {
+        delete currentVal._id;
+    });
+}
+
 function _sendAdminFeeds (res) {
     var db = mongoConnection.getDb();
 
     db.collection("authAttempts").find().toArray(function(err, items) {
         if (err) return res.json(err);
+
+        _cleanData(items);
 
         res.json({
             "feed": "authorization attempts",
